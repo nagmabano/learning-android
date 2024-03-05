5c7238ab2c4c119122a3d381cb181f73929375bb
Nagma Bano <bano.nagma.1@gmail.com>
2024-03-05 12:39:27 +0530

    boilerplate for scrolling activity

---
+++ app/src/main/java/com/nagma/learningandroid/ScrollingActivity.kt
@@ -0,0 +1,24 @@
+package com.nagma.learningandroid
+
+import android.os.Bundle
+import com.google.android.material.appbar.CollapsingToolbarLayout
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import com.nagma.learningandroid.databinding.ActivityScrollingBinding
+
+class ScrollingActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityScrollingBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityScrollingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(findViewById(R.id.toolbar))
+        binding.toolbarLayout.title = title
+
+    }
+}
