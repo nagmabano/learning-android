fb0f88751b2fa2aa9e259006c17141392abe030c
Nagma Bano <bano.nagma.1@gmail.com>
2024-03-05 15:59:53 +0530

    store and publish data in a view model

--- app/src/main/java/com/nagma/learningandroid/ScrollingViewModel.kt
+++ app/src/main/java/com/nagma/learningandroid/ScrollingViewModel.kt
@@ -1,15 +1,20 @@
 package com.nagma.learningandroid
 
 import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class ScrollingViewModel: ViewModel() {
 
+    private val _info: MutableLiveData<Int> = MutableLiveData()
+
     init {
+        _info.value = 0
         Log.i("ScrollingViewModel", "created")
     }
 
     fun loadData() {
-        Log.i("ScrollingViewModel", "load data")
+        _info.value = _info.value!! + 1
+        Log.i("ScrollingViewModel", "current value: ${_info.value}")
     }
 }
