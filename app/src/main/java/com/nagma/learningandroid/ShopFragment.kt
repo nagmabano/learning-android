1e66269aa1d1ff50c3ef4f33a574d58550654e7d
Nagma Bano <bano.nagma.1@gmail.com>
2024-03-06 17:20:47 +0530

    create fragments

---
+++ app/src/main/java/com/nagma/learningandroid/ShopFragment.kt
@@ -0,0 +1,17 @@
+package com.nagma.learningandroid
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class ShopFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_scrolling, container, false)
+    }
+}
