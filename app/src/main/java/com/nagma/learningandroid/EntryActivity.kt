518b744ad319100e609f0c14dd8fdffd61e5f502
Nagma Bano <nagma.bano@sap.com>
2024-03-06 12:22:23 +0530

    start activity with explicit intents

--- app/src/main/java/com/nagma/learningandroid/EntryActivity.kt
+++ app/src/main/java/com/nagma/learningandroid/EntryActivity.kt
@@ -1,11 +1,22 @@
 package com.nagma.learningandroid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.nagma.learningandroid.databinding.EntryMainBinding
 
 class EntryActivity : AppCompatActivity() {
+
+    private lateinit var binding: EntryMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.entry_main)
+        binding = EntryMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.entryButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+
+        }
     }
 }
